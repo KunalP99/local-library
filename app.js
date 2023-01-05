80c1ab8e59cb450c8a05bcf7dc48c30b227822ec
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-01-05 13:40:10 +0000

    Catalog route created which handles all routes correctly

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 var app = express();
 
 const mongoose = require("mongoose");
 const mongoDB =
-  "mongodb+srv://kunalp:c7NVJX92so1VeYBX@cluster0.aik0287.mongodb.net/local_library?retryWrites=true&w=majority";
+  "mongodb+srv://kunalp:c7NVJX92so1VeYBX@cluster0.yxuv5xl.mongodb.net/local_library?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
